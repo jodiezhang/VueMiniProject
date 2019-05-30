02cdf8668f90c29f1653df1a55e76721c10595bc
Judy Zhang <20080904@163.com>
2019-05-30 13:26:16 +0800

    add new component robot builder

---
+++ src/build/PartSelector.vue
@@ -0,0 +1,54 @@
+<template>
+   <div class="part">
+            <img :src="selectedRobot.leftArm.src" title="left arm"/>
+            <button @click="selectPreviousLeftArm()" class="prev-selector">&#9650;</button>
+            <button @click="selectNextLeftArm()" class="next-selector">&#9660;</button>
+            <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+   </div> 
+       
+</template>
+<script>
+import availableParts from '../data/parts';
+const parts=availablePart.heads;
+function getPreviousValiIndex(index, length){
+  const deprecatedIndex=index-1;
+  return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
+}
+
+function getNextValidIndex(index, length){
+  const incrementedIndex = index + 1;
+  return incrementedIndex > length - 1 ? 0 :incrementedIndex;
+}
+
+export default {
+     name:'RobotBuilder',
+     data(){
+       return {
+         availableParts,
+         cart:[],//we need to initialize it or else Vue can not detect its change
+         //Vue's change detection works by hooking into the getter and setter properties
+         //in the data function
+         selectedHeadIndex:0,
+         selectedLeftArmIndex:0,
+         selectedTorsoIndex:0,
+         selectedRightArmIndex:0,
+         selectedBaseIndex:0,
+       };
+     },
+     computed:{
+     
+     },
+     methods:{
+ 
+       selectPreviousPart(){
+         this.selectedPartIndex = getNextValidIndex
+              (this.selectedPartIndex,availableParts.heads.length);
+       },
+       selectNextPart(){
+         this.selectedPartIndex = getPreviousValiIndex
+              (this.selectedPartIndex,availableParts.heads.length);
+       },
+       
+     }
+ }
+</script>
---
+++ src/home/HomePage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png"
+        aria-hidden="true" />
+    </div>
+    <div class="get-started">
+      <a href="">Get started</a>  Building your first robot!
+    </div>   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+
+.robot {
+  height:300px;
+}
+
+</style>
+
+<!--
+component name should 2 words at minimum
+
+no need to restart server 
+Vue CLI is using webpack DevServer,including Hot Module Replacement
+
+this is a single-file Components
+
+this file is built by webpack to generate HTML,JavaScript and CSS
+
+A new page means a new component
+
+-->
