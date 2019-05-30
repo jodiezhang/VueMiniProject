9348afcc5fedd2118721c9cbe1916d8df4dfca5c
Judy Zhang <20080904@163.com>
2019-05-30 16:59:45 +0800

    linking to routed pages

--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -5,7 +5,7 @@
         aria-hidden="true" />
     </div>
     <div class="get-started">
-      <a href="">Get started</a>  Building your first robot!
+      <router-link to="/build">Get started</router-link>  Building your first robot!
     </div>   
   </div>
 </template>
