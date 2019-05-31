9e0386bbfed793c7d1a7e02da5c09015b3858098
Judy Zhang <20080904@163.com>
2019-06-01 00:47:26 +0800

    understand global and namespaced state state always be namespaced getters are not you can define the namespace for getters mutations actions if the child has no namespace for getters then the getters are shared with root local module state
    foo(state,getters,rootState)
    in a no namespaced module getters are getters of entire store
    in a namespaves module getters are getters in local modules/robots.js
    rootState is the parent module state
    
    mutation has no rootstate parameters

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -3,13 +3,18 @@ import axios from 'axios';
 export default {
   state: {
     user: null,
+    foo: 'users-foo',
   },
   mutations: {
     updateCurrentUser(state, user) {
       state.user = user;
     },
   },
-  getters: {},
+  getters: {
+    foo(state, getters, rootState) {
+      return `user-getter/${rootState.foo}`;
+    },
+  },
   actions: {
     signIn({ commit }) {
       axios.post('/api/sign-in')
