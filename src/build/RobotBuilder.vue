eaad0b75efc2e75b9661ae7758bf077e7c348d9e
Judy Zhang <20080904@163.com>
2019-05-31 23:35:14 +0800

    using Actions to save data to an API

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -100,7 +100,8 @@ export default {
                 robot.rightArm.cost + 
                 robot.base.cost;
                 console.log(cost);
-          this.$store.commit('addRobotToCart',Object.assign({},robot,{cost}));
+         // this.$store.commit('addRobotToCart',Object.assign({},robot,{cost}));
+         this.$store.dispatch('addRobotToCart',Object.assign({},robot,{cost}));
          // this.cart.push();       
         // this.cart.push(Object.assign({},robot,{cost})); 
          this.addedToCart=true;           
