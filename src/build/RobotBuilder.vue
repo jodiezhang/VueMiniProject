33abd0e024a84f057b99a130b66768a0a6458f75
Judy Zhang <20080904@163.com>
2019-05-31 23:39:45 +0800

    return promises from Actions and once addRobotToCart successfully route to cart page

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -101,7 +101,8 @@ export default {
                 robot.base.cost;
                 console.log(cost);
          // this.$store.commit('addRobotToCart',Object.assign({},robot,{cost}));
-         this.$store.dispatch('addRobotToCart',Object.assign({},robot,{cost}));
+         this.$store.dispatch('addRobotToCart',Object.assign({},robot,{cost}))
+         .then(()=>this.$router.push('/cart'));
          // this.cart.push();       
         // this.cart.push(Object.assign({},robot,{cost})); 
          this.addedToCart=true;           
