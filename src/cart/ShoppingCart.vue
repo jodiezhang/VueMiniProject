26f28df2254ff0cee5c3bb6303070b7deb5ac6e4
Judy Zhang <20080904@163.com>
2019-06-01 23:17:58 +0800

    global filter

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -49,12 +49,8 @@
     </div>
 </template>
 <script>
-import currencyFilter from '../shared/currency-filter';
 export default {
     name: 'Cart',
-    filters:{
-        currency:currencyFilter,
-    },
     computed:{
         cart(){
             return this.$store.state.robots.cart;
